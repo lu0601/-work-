f6aba38c26968d74cf56c87c55aa39518b3e1bcf
lu0601 <50568129+lu0601@users.noreply.github.com>
2019-06-17 20:13:13 +0800

    Update Entropy(in AI).cpp

--- Entropy(in AI).cpp
+++ Entropy(in AI).cpp
@@ -4,7 +4,7 @@ int main()
 {
     printf("Please input i1 ~ i4>\n");
     double i1=0, i2=0,i3=0,i4=0;//...
-    scanf("%lf%lf%lf%lf",&i1, &i2,&i3, &i4);//...如果要計算出0.25和0.25的entropy，輸入i1=1, i2=4, i3=1, i4=4 即可
+    scanf("%lf%lf%lf%lf",&i1, &i2,&i3, &i4);//...如果要計算出I(0.25,0.75)，輸入i1=1, i2=4, i3=3, i4=4 即可
     double result=0;
     result = (-((i1/i2)*log(i1/i2)) - (i3/i4)*log(i3/i4))/log(2);//...
     printf("%.3f",result);
